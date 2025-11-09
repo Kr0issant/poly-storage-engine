9822253258d1eb98c28c485c141fe98f5e839861
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2025-11-09 23:47:52 +0530

    add transparent replacement

--- server/test.py
+++ server/test.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import gridfs
 import cv2
+import numpy as np
 
 def save_video_frames(video_path: str, split_count: int):
     cap = cv2.VideoCapture(video_path)
@@ -30,8 +31,18 @@ def save_video_frames(video_path: str, split_count: int):
     cv2.destroyAllWindows()
     return
 
-def fill_transparent_with_white():
-    pass
+def fill_transparent_with_white(image):
+    img_array = np.array(image)
+    if img_array.shape[-1] == 4:
+        img_cv = cv2.cvtColor(img_array, cv2.COLOR_RGBA2BGRA)
+        b, g, r, a = cv2.split(img_cv)
+        img_bgr = cv2.merge([b, g, r])
+        white = np.ones_like(img_bgr, dtype=img_bgr.dtype) * 255
+        alpha_mask = a / 255.0
+        blended = (img_bgr * alpha_mask[:, :, np.newaxis] + white * (1 - alpha_mask)[:, :, np.newaxis])
+        return blended.astype(np.uint8)
+    else:
+        return cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
 
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -45,9 +56,12 @@ fs = gridfs.GridFSBucket(db)
 #     file_id = fs.upload_from_stream(filename="countdown2.mp4", source=file)
 #     print(file_id)
 
-with open("output.mp4", "wb") as f:
-    fs.download_to_stream(file_id=ObjectId("6910d362a3ab7cf106a163da"), destination=f)
+# with open("output.mp4", "wb") as f:
+#     fs.download_to_stream(file_id=ObjectId("6910d362a3ab7cf106a163da"), destination=f)
 
 # save_video_frames("countdown.mp4", 20)
+
+
+# cv2.imwrite(filename="output.png", img=fill_transparent_with_white(cv2.imread("transparent.png", cv2.IMREAD_UNCHANGED)))
 
 client.close()
