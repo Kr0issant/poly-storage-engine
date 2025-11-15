f8ee4937a2388469008c1c05af07a130adc3df05
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2025-11-15 13:58:13 +0530

    move unduplicated name logic to utility

--- server/modules/json_handler.py
+++ server/modules/json_handler.py
@@ -15,20 +15,19 @@ class JSONHandler():
             json_storage_list.append(self.get_shallow_copy(collection_obj=obj))
         return json_storage_list
     
-    def upload_json(self, data, filename):
+    def upload_json(self, data: dict, filename: str):
         json_skeleton = self.schema_handler.generate_schema(data)
         collection_name = self.schema_handler.existing_schema(json_skeleton)
 
         if collection_name == None:
             self.schema_handler.upload_schema(collection_name=filename, generated_schema=json_skeleton)
             print("Making new Schema")
-            collection_name = filename 
+            collection_name = filename
         
         collection = self.db[collection_name]
         print(f"File to be Saved in {collection_name}")
         
         if isinstance(data, dict):
-                print("File is a single object. Inserting 1 document...")
-                result = collection.insert_one(data)
-                print(f"Successfully inserted document with ID: {result.inserted_id}")
-    
+            print("File is a single object. Inserting 1 document...")
+            result = collection.insert_one(data)
+            print(f"Successfully inserted document with ID: {result.inserted_id}")
